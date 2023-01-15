9489ba53fbff03992ef62e5001b7753c9c800b4d
20boy <koyejooyerinde@gmail.com>
2023-01-16 00:17:23 +0100

    make better

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "axios";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [quote, setQuote] = useState("");
+  const getPost = () => {
+    axios
+      .get("https://api.quotable.io/random")
+      .then((result) => {
+        console.log(result);
+        setQuote(result.data.content);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <div className="App">
+        <header>
+          <h1>Random Quotes Generator from an API</h1>
+        </header>
+
+        <button onClick={getPost}>Get Quotes</button>
+        <p>{quote}</p>
+      </div>
+    </>
   );
 }
 
